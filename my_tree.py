08d28116d1833d5669b797aae5be9e80b22232d9
kovchyn <kovchyn@softserveinc.com>
2016-04-19 18:23:47 +0300

    Changed to right tree

--- my_tree.py
+++ my_tree.py
@@ -7,24 +7,82 @@
 # Вычислите важности признаков и найдите два признака с наибольшей важностью. Их названия будут ответами для данной задачи (в качестве ответа укажите названия признаков через запятую или пробел, порядок не важен).
 #
 
-import numpy as np
-import pandas as pd
+import numpy
+import pandas
 from sklearn.tree import DecisionTreeClassifier
 
+data = pandas.read_csv('./_titanic.csv', index_col='PassengerId')
+main_data_frame = pandas.DataFrame(data=data, columns=['Pclass', 'Fare', 'Age', 'Sex', 'Survived'])
+main_data_frame = main_data_frame[["Pclass", "Fare", "Age", "Sex", "Survived"]].dropna()
 
-data = pd.read_csv('_titanic.csv', index_col="PassengerId")
-df = pd.DataFrame(data, columns=['Pclass', 'Fare', 'Age', 'Sex'])
-
-df[df['Sex'] == 'male'] = 1
-df[df['Sex'] == 'female'] = 2
-
-#df = df.notnull()
-
-X = np.array(df)
-Y = np.array(pd.DataFrame(data, columns=['Survived']))
+filtered_df = main_data_frame[["Pclass", "Fare", "Age", "Sex"]].replace("female", 0).replace("male", 1)
 
 clf = DecisionTreeClassifier(random_state=241)
+
+Y = main_data_frame['Survived']
+X = filtered_df
+print(X.columns)
+
+
 clf.fit(X, Y)
 
-importances = clf.feature_importances_
-print(importances[0])
+print(clf.feature_importances_)
+
+# import numpy as np
+# import pandas as pn
+# from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+#
+#
+#
+# data = pn.read_csv('_titanic.csv', index_col='PassengerId')
+#
+#
+# filtered_data = data[["Pclass", "Fare", "Age", "Sex", "Survived"]].dropna()
+# goal_data = filtered_data["Survived"]
+#
+# clr = DecisionTreeClassifier(random_state=241)
+#
+#
+# # In[13]:
+#
+# mutated_filtered_data = filtered_data[["Pclass", "Fare", "Age", "Sex"]].replace("female", 0).replace("male", 1)
+#
+#
+# # In[14]:
+#
+# mutated_filtered_data.head()
+#
+#
+# # In[15]:
+#
+# goal_data.head()
+#
+#
+# # In[16]:
+#
+# clr = clr.fit(mutated_filtered_data, goal_data)
+#
+#
+# # In[17]:
+#
+# importances = clr.feature_importances_
+#
+#
+# # In[18]:
+#
+# importances
+#
+#
+# # In[19]:
+#
+# clr.get_params()
+#
+#
+# # In[20]:
+#
+# print(list(zip(mutated_filtered_data.columns, clr.feature_importances_)))
+#
+#
+# # In[ ]:
+
+
